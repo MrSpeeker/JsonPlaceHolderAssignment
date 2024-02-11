393fcc83ab79e97f1062c6d56f9b5b34d7ecc27b
V van Rooyen <v.van.rooyen@cimsolutions.nl>
2024-02-11 20:30:27 +0100

    Styling changes

--- src/app/ui/card/card.component.ts
+++ src/app/ui/card/card.component.ts
@@ -1,23 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { AfterContentInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { Store } from '@ngrx/store';
 
 import { IJsonPlaceHolder } from '../../models/json-place-holder.interface';
 import * as JsonActions from '../../store/json-place-holder.action';
-import { jsonPlaceHolderEnum } from 'src/app/models/json-place-holder.type';
 
 @Component({
   selector: 'project-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatIconModule],
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
   @Input() public json!: IJsonPlaceHolder;
+  @Input() public changeColor = false;
   @Output() public itemChangeEvent: EventEmitter<number> =
     new EventEmitter<number>();
+
+  public isHovered = false;
 
   constructor(private _store: Store) {}
 
@@ -27,5 +30,25 @@ export class CardComponent {
     );
 
     this.itemChangeEvent.emit(this.json.id);
+  }
+
+  public changeValue() {
+    if (this.json.itemType === 0) {
+      return this.json.title;
+    } else if (this.json.itemType === 1) {
+      return this.json.userId;
+    } else if (this.json.itemType === 2) {
+      return this.json.id;
+    } else {
+      return this.json.body;
+    }
+  }
+
+  public showText() {
+    this.isHovered = true;
+  }
+
+  public hideText() {
+    this.isHovered = false;
   }
 }
